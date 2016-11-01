1dccf5bcb586d3b8e9907922b1474c7d14fdf722
Risa Harada <supikateriburu@gmail.com>
2016-11-01 10:47:58 +0900

    :shower:
    Fix indent

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default class TodoBox extends React.Component{
   render() {
     return <div className="todoBox">
-        Hello, world!
-      </div>
+      Hello, world!
+    </div>
   }
 }
