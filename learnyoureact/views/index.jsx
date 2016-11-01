eb339ee4be56508fc09d604eeab2918cfc034d08
Risa Harada <supikateriburu@gmail.com>
2016-11-01 15:37:36 +0900

    :tada: Finish proptypes

--- learnyoureact/views/index.jsx
+++ learnyoureact/views/index.jsx
@@ -16,10 +16,11 @@ class TodoList extends React.Component {
   render() {
     return (
       <div className="todoList">
-        <table style={{ border: "2px solid black" }}>
+        <table style={{border: "2px solid black"}}>
           <tbody>
             <Todo title="Shopping">Milk</Todo>
             <Todo title="Hair cut">13:00</Todo>
+            <Todo title="Learn React">15:00</Todo>
           </tbody>
         </table>
       </div>
@@ -31,12 +32,16 @@ class Todo extends React.Component {
   render() {
     return (
       <tr>
-      <td style={{ border: "1px solid black" }}>{this.props.title}</td>
-      <td style={{ border: "1px solid black" }}>{this.props.children}</td>
+        <td style={{border: "1px solid black"}}>{this.props.title}</td>
+        <td style={{border: "1px solid black"}}>{this.props.children}</td>
       </tr>
-    )
+    );
   }
 }
+
+Todo.propTypes = {
+  title: React.PropTypes.number.isRequired
+};
 
 class TodoForm extends React.Component {
   render() {
