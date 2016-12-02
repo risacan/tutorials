42515f816d302ba223a52568f796cb28dbdbe7b0
Risa Harada <supikateriburu@gmail.com>
2016-12-02 11:21:03 +0900

    :white_check_mark: Easiest one

---
+++ karma/README.md
@@ -0,0 +1,1 @@
+Tried this [フロントエンドにテストを導入](http://qiita.com/howdy39/items/cdd5b252096f5a2fa438)
---
+++ karma/test/calc-util-spec.js
@@ -0,0 +1,8 @@
+console.info("test add function")
+console.info("1+2 equals to 3")
+
+if (add(1, 2) === 3) {
+  console.info("yay")
+} else {
+  console.error("nay")
+}
