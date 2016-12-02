23a9b834cf0852d5770268b339817826d6a7ce18
Risa Harada <supikateriburu@gmail.com>
2016-12-02 11:25:15 +0900

    :white_check_mark: Show the result on browser

--- karma/test/calc-util-spec.js
+++ karma/test/calc-util-spec.js
@@ -1,8 +1,14 @@
-console.info("test add function")
-console.info("1+2 equals to 3")
-
-if (add(1, 2) === 3) {
-  console.info("yay")
-} else {
-  console.error("nay")
-}
+describe('add関数のテスト', function() {
+    it('1+2は3である', function() {
+        if (add(1, 2) === 3) {
+        } else {
+            throw new Error('失敗');
+        }
+    });
+    it('1+2は4である', function() {
+        if (add(1, 2) === 4) {
+        } else {
+            throw new Error('失敗');
+        }
+    });
+});
